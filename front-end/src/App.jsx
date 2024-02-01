88f29d0e5064959eb020a228ce8d736f381a6deb
mohd-ashif <mohdaszif077@gmail.com>
2024-02-01 16:15:20 +0530

    add bootstrap

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -4,6 +4,9 @@ import './index.css';
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import HomeScreen from './Screen/HomeScreen';
 import ProductScreen from './Screen/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar'
+import Container  from 'react-bootstrap/Container'
+import {LinkContainer} from 'react-router-bootstrap'
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,17 +15,31 @@ function App() {
     <>
       <BrowserRouter>
        
-        <div>
-          <header>
-            <Link to="/">Amazona</Link>
+        <div className='d-flex flex-column site-container' >
+          <header  >
+            <Navbar bg='dark' variant='dark'>
+              <Container>
+                
+                  <LinkContainer to='/'>
+                  <Navbar.Brand> amazona</Navbar.Brand>
+                  </LinkContainer>
+              
+              </Container>
+            </Navbar>
+
+           
           </header>
           <main>
+            <Container>
           <Routes>
             <Route path='/product/:slug' element={<ProductScreen />} />
           <Route path="/" element={<HomeScreen />} />
         </Routes>
-
+        </Container>
           </main>
+          <footer>
+           <div className='text-center' > all rights reserved</div>
+          </footer>
         </div>
       </BrowserRouter>
     </>
