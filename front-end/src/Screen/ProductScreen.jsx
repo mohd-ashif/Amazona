b8e1464caa5baf699834faa7471289e67c05dc60
mohd-ashif <mohdaszif077@gmail.com>
2024-02-01 13:20:17 +0530

    add produt screen

--- front-end/src/Screen/ProductScreen.jsx
+++ front-end/src/Screen/ProductScreen.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+
 
 const ProductScreen = () => {
+
+    const params = useParams()
+    const {slug} = params
   return (
-    <div>ProductScreen</div>
+    <div>
+        <h1>{slug}</h1>
+    </div>
   )
 }
 
