f4317fd127a353e6503a9ffa96fff2b4e5aa8928
mohd-ashif <mohdaszif077@gmail.com>
2024-02-01 13:17:08 +0530

    add home scree  & Routes

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,39 +1,30 @@
 import React, { useState } from 'react';
 import './App.css';
 import './index.css';
-import data from './data';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import HomeScreen from './Screen/HomeScreen';
+import ProductScreen from './Screen/ProductScreen';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <header>
-          <a href="/">Amazona</a>
-        </header>
-        <main>
-          <h1>Featured Products</h1>
-          <div className="products">
-          {data.products.map((product) => (
-            <div className="product" key={product.slug}>
-              <a href={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </a>
-              <div className="product-info">
-                <a href={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </a>
-                <p>
-                  <strong>${product.price}</strong>
-                </p>
-                <button>Add to cart</button>
-              </div>
-            </div>
-          ))}
+      <BrowserRouter>
+       
+        <div>
+          <header>
+            <Link to="/">Amazona</Link>
+          </header>
+          <main>
+          <Routes>
+            <Route path='/product/:slug' element={<ProductScreen />} />
+          <Route path="/" element={<HomeScreen />} />
+        </Routes>
+
+          </main>
         </div>
-        </main>
-      </div>
+      </BrowserRouter>
     </>
   );
 }
