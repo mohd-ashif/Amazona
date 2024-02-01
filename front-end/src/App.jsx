05ad7d69de42602ef71d2c38c5d428c4e9fb11d3
mohd-ashif <mohdaszif077@gmail.com>
2024-02-01 12:25:37 +0530

    add product list

---
+++ front-end/src/App.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import './App.css';
+import './index.css';
+import data from './data';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <header>
+          <a href="/">Amazona</a>
+        </header>
+        <main>
+          <h1>Featured Products</h1>
+          <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default App;
