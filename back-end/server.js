3ac6a8f217a2996a3b337129757d8f59e75ec934
mohd-ashif <mohdaszif077@gmail.com>
2024-03-10 11:45:18 +0530

    add origin

--- back-end/server.js
+++ back-end/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // Enable CORS globally
 app.use(cors({
-  origin :[" http://localhost:3000", 'http://ecommerce-mern-app.render.com']
+  origin :[" http://localhost:5000", 'http://ecommerce-mern-app.render.com']
 }));
 
 app.get('/', (req, res) => {
